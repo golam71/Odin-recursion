6cb3108c90a6f9b2d3dd8d43054b0e6401dda80d
golam71 <74001687+golam71@users.noreply.github.com>
2025-06-27 15:14:34 +0600

    :zap: Add Fibonacci function implementation and example usage

---
+++ merge.js
@@ -0,0 +1,12 @@
+// function fib(n) {
+//   let x = 0;
+//   let y = 1;
+//   let result = [];
+//   for (let i = 0; i <= n; i++) {
+//     result.push(x);
+//     [x, y] = [y, x + y];
+//   }
+//   return result;
+// }
+
+// console.log(fib(8));
